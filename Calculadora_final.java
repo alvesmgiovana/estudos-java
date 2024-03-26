20f7fe2ff56d9629e404b369f882bbdce216e9bd
Giovana Magalhães Alves <gi.alves.magalhaes@gmail.com>
2024-03-25 21:17:02 -0300

    Finalizada completo

--- Calculadora_final.java
+++ Calculadora_final.java
@@ -1,17 +1,22 @@
+import javax.swing.JOptionPane;
+
 public class Calculadora_final {
     public static void main(String[] args) {
         Calculadora calc = new Calculadora();
 
-        double som = calc.somar(10, 5);
+        double a = Double.parseDouble(JOptionPane.showInputDialog("Insira o primeiro valor: "));
+        double b = Double.parseDouble(JOptionPane.showInputDialog("Insira o segundo valor: "));
+
+        double som = calc.somar(a, b);
         System.out.println(som);
 
-        double sub = calc.subtrair(10, 5);
+        double sub = calc.subtrair(a, b);
         System.out.println(sub);
 
-        double mult = calc.multiplicar(10, 5);
+        double mult = calc.multiplicar(a, b);
         System.out.println(mult);
 
-        double div = calc.dividir(10, 5);
+        double div = calc.dividir(a, b);
         System.out.println(div);
     }
 }
--- calculadora.java
+++ calculadora.java
@@ -1,9 +1,6 @@
 import javax.swing.JOptionPane;
 
 public class Calculadora {
-    double a = Double.parseDouble(JOptionPane.showInputDialog("Insira o primeiro valor: "));
-    double b = Double.parseDouble(JOptionPane.showInputDialog("Insira o segundo valor: "));
-
     public double somar(double a, double b) {
         return a + b;
     }
